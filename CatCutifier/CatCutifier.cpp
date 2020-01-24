1d15cb6fd71789e744cd45f906d241d6ebf1fb1e
furkanx007 <furkanguvenc1998@gmail.com>
2020-01-24 19:14:30 +0300

    first commit

---
+++ CatCutifier/CatCutifier.cpp
@@ -0,0 +1,8 @@
+﻿// CatCutifier.cpp : Defines the entry point for the application.
+//
+
+#include "CatCutifier.h"
+
+void cat::make_cute() {
+    // Magic happens
+}
---
+++ CatCutifier/CatCutifier.h
@@ -0,0 +1,15 @@
+﻿// CatCutifier.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+/**
+  A fluffy feline
+*/
+struct cat {
+    /**
+      Make this cat look super cute
+    */
+    void make_cute();
+};
+// TODO: Reference additional headers your program requires here.
